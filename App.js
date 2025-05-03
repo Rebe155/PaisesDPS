d64c9a49c0faa1659bef5afe1943d86e0418e5f3
Rebe155 <arevalorebeca41@gmail.com>
2025-05-03 10:06:31 -0600

    subiendo proyecto paises

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Formulario from './src/components/Formulario';
+import Pais from './src/components/Pais';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [busqueda, guardarbusqueda] = useState({ pais: '' });
+    const [consultar, guardarconsultar] = useState(false);
+    const [resultado, guardarresultado] = useState({});
+
+    useEffect(() => {
+        const {pais} = busqueda;
+        const consultarPais = async () => {
+           if (consultar) {
+                const url = `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+                try {
+                    const respuesta = await fetch(url);
+                    const resultado = await respuesta.json();
+                    guardarresultado(resultado);
+                    guardarconsultar (false);
+                } catch (error) {
+                    mostrarAlerta();
+                }
+            }
+        };
+        consultarPais();
+    }, [consultar]);
+
+    const mostrarAlerta = () => {
+        Alert.alert('Error', 'No hay resultados, intenta con otro país'), 
+          [{ text: 'Ok' }];
+    };
+
+    return (
+        <View style={styles.app}>
+            <View style={styles.contenido}>
+                <View style={{ zIndex: 1000 }}>
+                    <Formulario
+                        busqueda={busqueda}
+                        guardarbusqueda={guardarbusqueda}
+                        guardarconsultar={guardarconsultar}
+                    />
+                </View>
+                <View style={{ zIndex: 1 }}>
+                    <Pais resultado={resultado} />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    app: {
+        flex: 1,
+        backgroundColor: 'rgb(71,149,212)',
+        justifyContent: 'center',
+    },
+    contenido: {
+        margin: '2.5%',
+    }
 });
